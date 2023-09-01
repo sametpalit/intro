58c9f40f05eead5f72948e1a05116a31b0053195
Samet <sametpalit@live.nl>
2023-09-01 12:23:38 +0200

    4e commit

---
+++ projectpagina/project.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Your Portfolio</title>
+</head>
+<body>
+  <nav>
+    <ul>
+      <li><a href="../index.html">Home</a></li>
+      <li><a href="/about/about.html">About</a></li>
+      <li><a href="project.html"class='current-page'>Projects</a></li>
+      <li><a href="/contact/contact.html">Contact</a></li>
+    </ul>
+  </nav>
+  
+  <header>
+    <h1>Samet Palit</h1>
+    <img src="fener.jpeg" id="image" width="600px">
+  </header>
+  
+  <main>
+    <section>
+      <h2>Introductie</h2>
+      <div class="introductie">
+        <p>Hallo daar! Ik ben Samet, een enthousiaste student en software developer. Ontdek mijn projecten en mijn reis in code op dit portfolio.</p>
+      </div>
+    </section>
+  </main>
+  
+  <footer>
+    <p>&copy; 2023 Samet Palit. All rights reserved.</p>
+  </footer>
+</body>
+</html>
